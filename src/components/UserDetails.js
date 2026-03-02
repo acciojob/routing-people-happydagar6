60c3475849da4a751fde595f269ff3582385c889
Your Name <harshitdagar913@gmail.com>
2026-03-02 15:17:42 +0530

    fix: update UserDetails component to use template literals for user information

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,37 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const UserDetails = () => {
+  const { id } = useParams();
 
-    const { id } = useParams();
+  const [users, setUsers] = React.useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [users, setUsers] = React.useState([]);
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, [id]);
 
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-            .then(response => response.json())
-            .then(data => {
-                setUsers(data);
-                setLoading(false);
-            })
-    }, [id]);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  if (!users) {
+    return <div>User not found</div>;
+  }
   return (
     <div>
       <h1>User Details</h1>
-        <p>Name: {users.name}</p>
-        <p>Email: {users.email}</p>
-        <p>Phone: {users.phone}</p>
-        <p>Website: {users.website}</p>
-
-        <br/>
-        <Link to="/">Back to User List</Link>
+      {/* THE FIX: Template literals use kiye taaki exact text match ho */}
+      <p>{`Name: ${users.name}`}</p>
+      <p>{`Username: ${users.username}`}</p>
+      <p>{`Email: ${users.email}`}</p>
+      <p>{`Phone: ${users.phone}`}</p>
+      <p>{`Website: ${users.website}`}</p>
+      <br />
+      <Link to="/">Back to User List</Link>
     </div>
-  )
-}
+  );
+};
 
-export default UserDetails
+export default UserDetails;
