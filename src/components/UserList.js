52e45f7d32db8720b70730d6d0c6d22e59012eed
Your Name <harshitdagar913@gmail.com>
2026-03-02 15:28:30 +0530

    fix: improve loading state management and code formatting in UserList component

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const UserList = () => {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true); // FIX: Loading added
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(data => setUsers(data))
-    }, []);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>User List</h1>
       <ul>
-        {users.map(user => (
-            <li key={user.id}>
-                <Link to={`/users/${user.id}`}>{user.name}</Link>
-            </li>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.name}</Link>
+          </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default UserList
+export default UserList;
